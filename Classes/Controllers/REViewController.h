7051b0c8dbd0e95dc57df322ff7115a9b8ac6f0e
ojimac <ojimac@gmail.com>
2013-03-02 19:34:35 +0900

    GoogleMapサンプル表示

--- Classes/Controllers/REViewController.h
+++ Classes/Controllers/REViewController.h
@@ -1,2 +1,8 @@
+// Pods
+#import "GoogleMaps.h"
+
 @interface REViewController : UIViewController
+
+@property (strong, nonatomic) GMSMapView *mapView;
+
 @end
--- Classes/Controllers/REViewController.m
+++ Classes/Controllers/REViewController.m
@@ -5,13 +5,24 @@
 
 @implementation REViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
+
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.8683
+                                                            longitude:151.2086
+                                                                 zoom:6];
+    _mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    _mapView.myLocationEnabled = YES;
+    self.view = _mapView;
+
+    GMSMarkerOptions *options = [[GMSMarkerOptions alloc] init];
+    options.position = CLLocationCoordinate2DMake(-33.8683, 151.2086);
+    options.title = @"Sydney";
+    options.snippet = @"Australia";
+    [_mapView addMarkerWithOptions:options];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
